f6b7c0f1b6fa909a5e9ee9e9dddeff3e496c511e
Honza Javorek <jan.javorek@gmail.com>
2012-10-10 15:13:29 +0200

    non-working (due to access tokens) facebook API check on Rubyslava events

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 (function($){
      $.extend(
          {
-            escape: function (str) {
+            _escape: function (str) {
                 return str.replace(/"/g, '\'');
             },
             scrape: function (url, xpath, successCallback, errorCallback) {
                 var query = 'SELECT * FROM html WHERE url="' +
-                    this.escape(url) + '" AND xpath="' +
-                    this.escape(xpath) + '"';
+                    this._escape(url) + '" AND xpath="' +
+                    this._escape(xpath) + '"';
 
                 var ajaxSettings = {
                     'url': 'http://query.yahooapis.com/v1/public/yql',
@@ -33,20 +33,44 @@
      );
  })(jQuery);
 
-$(function() {
+$(document).ready(function() {
+    // Prague
     var $prague = $('.prague');
     var pragueSrc = 'http://lanyrd.com/series/django-cs/';
 
-    $.scrape(pragueSrc, '//*[contains(@class, "conference-listing")]//h4', function (data) {
-        var url = 'http://lanyrd.com' + data.query.results.h4[0].a.href;
+    $.scrape(pragueSrc, '//*[contains(@class, "conference-listing")]//h4', function (response) {
+        var url = 'http://lanyrd.com' + response.query.results.h4[0].a.href;
         $('h3 a', $prague).attr('href', url);
     });
 
-    $.scrape(pragueSrc, '//*[contains(@class, "dtstart")]', function (data) {
-        var date = data.query.results.abbr[0].title;
+    $.scrape(pragueSrc, '//*[contains(@class, "dtstart")]', function (response) {
+        var date = response.query.results.abbr[0].title;
         date = date.split('-');
         date = date[2] + '. ' + date[1] + '.';
 
         $('.date', $prague).text(date);
+    });
+
+    // Brno
+    /*
+
+        TODO
+
+    */
+});
+
+$(document).on('fbload', function() {
+    // Bratislava
+    // var uid = 666004183; // Ján Suchal
+    var uid = 721972706; // Honza Javorek
+    var fql = 'SELECT name FROM event WHERE eid IN ' +
+        '(SELECT eid FROM event_member WHERE uid = ' + uid + ')';
+
+    FB.api('/fql?q=' + encodeURIComponent(fql), function(response) {
+        console.log(response);
+    });
+
+    FB.api('/721972706', function(response) {
+        console.log(response);
     });
 });
