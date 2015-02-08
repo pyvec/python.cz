a9172f4f241d2f823101d6ad75e2731953928409
Honza Javorek <mail@honzajavorek.cz>
2015-02-08 20:23:36 +0100

    Initial version of texts and plain HTML for both Czech and English homepages of the new site.

--- web.py
+++ web.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask, render_template, url_for, redirect
+from flask import (Flask, render_template as _render_template, url_for,
+                   redirect, request)
 
 
+# This settings ensure that Flask templates are in the root of the application
 app = Flask(__name__,
             static_folder='.',
             static_url_path='',
             template_folder='.')
 
 
+def render_template(filename, **kwargs):
+    kwargs['github_url'] = (
+        'https://github.com/pyvec/python.cz/blob/master/' + filename
+    )
+    return _render_template(filename, **kwargs)
+
+
 @app.context_processor
 def inject_context():
     return {
-        'debug': app.debug
+        'debug': app.debug,
+        'url': 'http://python.cz' + request.path
     }
 
 
@@ -27,9 +37,14 @@ def index_legacy():
     return redirect(url_for('index'), code=301)
 
 
-@app.route('/english.html')
+@app.route('/index-en.html')
 def index_en():
-    return render_template('english.html')
+    return render_template('index-en.html')
+
+
+@app.route('/english.html')
+def index_en_legacy():
+    return redirect(url_for('index_en'), code=301)
 
 
 if __name__ == '__main__':
