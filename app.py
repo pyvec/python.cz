099296fbddee6e5203b3ea8714f010ba5ddf2476
Honza Javorek <mail@honzajavorek.cz>
2015-08-30 18:52:16 +0200

    Deployment.

--- app.py
+++ app.py
@@ -1,28 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Configuration for deploying python.cz on the rosti.cz hosting.
 
-It should be easy to adapt to other kinds of WSGI-based hosting.
-
-
-Instructions:
-- Put this repo in /srv/app. You'll need to clear the previous contents:
-
-        rm -rf /srv/app
-        git clone https://github.com/honzajavorek/python.cz /srv/app
-
-- Install everything:
-
-        cd /srv/app
-        pip install -r requirements.txt
-
-- Restart:
-
-        supervisorctl restart app
-
-"""
-
-
-from pythoncz import app as application  # noqa
+from pythoncz import app as application  # NOQA
---
+++ deployment/update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Update of app source code, dependencies installation, app restart
+#
+# This script is designed to be remotely executed by TravisCI build
+# as part of automatic continuous deployment.
+
+# This script should be executed remotely on the production machine.
+if [ "$USER" != "app" ]; then
+    exit
+fi
+
+# clear existing source code
+rm -rf /srv/app
+
+# get latest code
+git clone --progress --depth=1 https://github.com/honzajavorek/python.cz /srv/app
+
+# install dependencies
+/srv/venv/bin/pip install -r /srv/app/requirements.txt
+
+# restart the app
+supervisorctl restart app
