b59ae8884272ee85d3df1de8663ca6a32f6098f4
Honza Javorek <mail@honzajavorek.cz>
2015-09-01 20:29:04 +0200

    Checking JSON and YAML files.

--- tests/valid_yaml_test.py
+++ tests/valid_yaml_test.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 
-import json
+import yaml
 from os import path
 from glob import glob
 
 
 def test_valid_json():
-    """Tests whether all JSON data files are valid JSON documents."""
+    """Tests whether all YAML data files are valid YAML documents."""
 
     test_dir = path.dirname(path.abspath(__file__))
     data_dir = path.join(test_dir, '..', 'static', 'data')
 
-    for filename in glob(path.join(data_dir, '*.*json')):
+    for filename in glob(path.join(data_dir, '*.yml')):
         with open(filename) as f:
-            assert json.load(f)
+            assert yaml.load(f.read())
