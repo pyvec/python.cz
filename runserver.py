ba41e40ecc0291ee22a0e531dd8a72613f9b815f
Honza Javorek <mail@honzajavorek.cz>
2016-03-23 23:12:21 +0100

    Add SERVER_NAME

--- runserver.py
+++ runserver.py
@@ -8,5 +8,8 @@ from pythoncz import app
 
 
 if __name__ == '__main__':
+    host = '0.0.0.0'
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+
+    app.config['SERVER_NAME'] = '{host}:{port}'.format(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
