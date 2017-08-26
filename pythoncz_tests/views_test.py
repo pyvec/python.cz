0bc711b981286d157e6963486283575867beca47
Honza Javorek <mail@honzajavorek.cz>
2017-08-26 19:57:24 +0200

    Make the tests more understandable

--- pythoncz_tests/views_test.py
+++ pythoncz_tests/views_test.py
@@ -8,7 +8,13 @@ from pythoncz import app
 from pythoncz.models import github as github_module
 
 
-def create_issue(**kwargs):
+def generate_issue_mock(**kwargs):
+    """
+    Generates an issue fixture
+
+    All attributes can be overriden by keyword arguments for the purpose
+    of testing.
+    """
     issue = {
         'title': 'Test Issue',
         'html_url': 'http://github.com/pyvec/zapojse/issues/42',
@@ -33,8 +39,19 @@ def create_issue(**kwargs):
     return issue
 
 
+def extract_issues_html(html):
+    """
+    Extracts the part of the HTML page, which contains the list of issues
+
+    Testing the relevant part avoids getting false positives/negatives
+    due to certain texts being present also in other parts of the page.
+    """
+    return html.split('id="issues"')[1]
+
+
 @pytest.fixture
 def test_client():
+    """Flask app test client"""
     app.testing = True
     with app.test_client() as client:
         yield client
@@ -56,70 +73,84 @@ def github():
     github_module.get_issues = original_get_issues
 
 
-def test_get_involved_cs_no_issues(github, test_client):
-    response = test_client.get('/zapojse/')
-
-    assert response.status_code == 200
-
-
-def test_get_involved_cs(github, test_client):
+def test_get_involved_cs_renders_ordinary_issue(github, test_client):
     def get_issues(self, *args, **kwargs):
-        return [create_issue()]
+        return [generate_issue_mock()]
     github.get_issues = get_issues
+
     response = test_client.get('/zapojse/')
-    html = response.get_data(as_text=True)
+    html = extract_issues_html(response.get_data(as_text=True))
 
+    assertions = [
+        ('Test Issue', 'Issue title'),
+        ('http://github.com/pyvec/zapojse/issues/42', 'Issue URL'),
+        ('https://github.com/encukou', 'Issue author URL'),
+        ('od encukou', 'Issue author'),
+        ('komentářů: 5', 'Comments count'),
+        ('účastníků diskuze: 6', 'Participants count'),
+        ('hlasů: 3', 'Votes count'),
+    ]
     assert response.status_code == 200
-    assert 'Test Issue' in html
-    assert 'http://github.com/pyvec/zapojse/issues/42' in html
-    assert 'https://github.com/encukou' in html
-    assert 'od encukou' in html
-    assert 'komentářů: 5' in html
-    assert 'účastníků diskuze: 6' in html
-    assert 'hlasů: 3' in html
-    assert 'fa-code-fork' not in html
+    for expectation, description in assertions:
+        assert expectation in html, description + " isn't present in the HTML"
 
 
-@pytest.mark.parametrize('label_name,text,occurrences', [
-    ('coach', 'Kouč pomůže!', 2),  # because it's mentioned also in the legend
-    ('sprint-idea', 'Sprint', 1),
+@pytest.mark.parametrize('label_name,label_text', [
+    ('coach', 'Kouč pomůže!'),
+    ('sprint-idea', 'Sprint'),
 ])
-def test_get_involved_cs_labels(github, test_client,
-                                label_name, text, occurrences):
+def test_get_involved_cs_renders_special_labels(github, test_client,
+                                                label_name, label_text):
+    """
+    Some GitHub issue labels have a special meaning and the 'get_involved_cs'
+    page promotes them
+    """
     def get_issues(self, *args, **kwargs):
-        return [create_issue(**{'labels': [label_name], label_name: True})]
+        return [generate_issue_mock(**{
+            'labels': [label_name],
+            label_name: True,
+        })]
     github.get_issues = get_issues
+
     response = test_client.get('/zapojse/')
-    html = response.get_data(as_text=True)
+    html = extract_issues_html(response.get_data(as_text=True))
 
-    assert html.count(text) == occurrences
+    assert response.status_code == 200
+    assert label_text in html, "Special label isn't present in the HTML"
 
 
-@pytest.mark.parametrize('repo_name,repo_linked,label', [
+@pytest.mark.parametrize('repo_name,is_rendered,label_cls', [
     ('pyvec/zapojse', False, None),
     ('pyvec/repo', True, None),
     ('pyladiescz/repo', True, 'label-pyladies'),
 ])
-def test_get_involved_cs_repos(github, test_client,
-                               repo_name, repo_linked, label):
+def test_get_involved_cs_renders_repo_name(github, test_client,
+                                           repo_name, is_rendered, label_cls):
+    """
+    If the issue (or pull request) is not from the pyvec/zapojse repository,
+    it is preceeded by a link to its repository. If the issue is from the
+    PyLadiesCZ GitHub organization, it is labeled with a 'PyLadies' label
+    """
     def get_issues(self, *args, **kwargs):
-        return [create_issue(
+        return [generate_issue_mock(
             html_url='http://github.com/{}/issues/42'.format(repo_name),
             repository_name=repo_name.split('/')[1],
             repository_url_html='http://github.com/{}/'.format(repo_name),
             organization_name=repo_name.split('/')[0],
         )]
     github.get_issues = get_issues
-    response = test_client.get('/zapojse/')
-    html = response.get_data(as_text=True)
 
-    if repo_linked:
-        assert 'fa-code-fork' in html
-        assert 'http://github.com/{}/'.format(repo_name) in html
+    response = test_client.get('/zapojse/')
+    html = extract_issues_html(response.get_data(as_text=True))
+
+    if is_rendered:
+        assert 'fa-code-fork' in html, "Repo icon isn't present in the HTML"
+        url = 'http://github.com/{}/'.format(repo_name)
+        assert url in html, "Repo URL isn't present in the HTML"
     else:
-        assert 'fa-code-fork' not in html
-    if label:
-        assert label in html
+        assert 'fa-code-fork' not in html, "Repo icon is present in the HTML"
+    if label_cls:
+        assert label_cls in html, "Label isn't present in the HTML"
 
 
 @pytest.mark.parametrize('is_pull_request,icon_name', [
@@ -128,47 +159,60 @@ def test_get_involved_cs_repos(github, test_client,
 ])
 def test_get_involved_cs_pull_request_icon(github, test_client,
                                            is_pull_request, icon_name):
+    """
+    Pull Requests have a different icon than Issues
+    """
     def get_issues(self, *args, **kwargs):
-        return [create_issue(is_pull_request=is_pull_request)]
+        return [generate_issue_mock(is_pull_request=is_pull_request)]
     github.get_issues = get_issues
+
     response = test_client.get('/zapojse/')
-    html = response.get_data(as_text=True)
+    html = extract_issues_html(response.get_data(as_text=True))
 
     assert icon_name in html
 
 
-def test_get_involved_cs_error(github, test_client):
+def test_get_involved_cs_handles_error(github, test_client):
+    """
+    If error happens when generating the 'get_involved_cs' page, the view
+    should handle it and still display most of the content. The issues
+    section should contain an error message with some useful links
+    """
     def get_issues(self, *args, **kwargs):
         raise RuntimeError('Ouch!')
     github.get_issues = get_issues
+
     response = test_client.get('/zapojse/')
-    html = response.get_data(as_text=True)
+    html = extract_issues_html(response.get_data(as_text=True))
 
     assert response.status_code == 500
-    assert 'issues-error' in html
-    assert 'https://github.com/pyvec/zapojse/issues' in html
-    assert '{base_url}?title={title}&amp;body={body}'.format(
+    message = "DIV with the 'issues-error' class isn't present in the HTML"
+    assert 'issues-error' in html, message
+    message = "Link to alternative issues listing isn't present in the HTML"
+    assert 'https://github.com/pyvec/zapojse/issues' in html, message
+    url = '{base_url}?title={title}&amp;body={body}'.format(
         base_url='https://github.com/pyvec/python.cz/issues/new',
         title=url_quote_plus('Nefunguje Zapoj se'),
         body=url_quote_plus('RuntimeError: Ouch!'),
-    ) in html
+    )
+    assert url in html, "URL for filing a bug report isn't present in the HTML"
 
 
-def test_index_legacy(test_client):
+def test_index_legacy_redirect(test_client):
     response = test_client.get('/index.html')
 
     assert response.status_code == 301
     assert response.headers['location'] == url_for('index_cs', _external=True)
 
 
-def test_index_en_legacy(test_client):
+def test_index_en_legacy_redirect(test_client):
     response = test_client.get('/english.html')
 
     assert response.status_code == 301
     assert response.headers['location'] == url_for('index_en', _external=True)
 
 
-def test_pyladies(test_client):
+def test_pyladies_redirect(test_client):
     response = test_client.get('/pyladies/foo/bar/baz.html')
 
     assert response.status_code == 301
@@ -177,14 +221,14 @@ def test_pyladies(test_client):
     )
 
 
-def test_pyladies_index_trailing_slash(test_client):
+def test_pyladies_index_trailing_slash_redirect(test_client):
     response = test_client.get('/pyladies/')
 
     assert response.status_code == 301
     assert response.headers['location'] == 'http://pyladies.cz'
 
 
-def test_pyladies_index_without_trailing_slash(test_client):
+def test_pyladies_index_without_trailing_slash_redirect(test_client):
     response = test_client.get('/pyladies')
 
     assert response.status_code == 301
@@ -193,7 +237,7 @@ def test_pyladies_index_without_trailing_slash(test_client):
     )
 
 
-def test_talks(test_client):
+def test_talks_redirect(test_client):
     response = test_client.get('/talks/foo/bar/baz.html')
 
     assert response.status_code == 301
