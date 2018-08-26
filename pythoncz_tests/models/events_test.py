5c2d61dde0c2199fefda409751c28fb0fb1f9f21
Mikuláš Poul <mikulaspoul@gmail.com>
2018-08-26 17:54:23 +0200

    Downloading event feeds using @app.before_first_request, separated preprocessing of iCal to a function and added a test for it, fixed some typos, renamed feeds file and other small fixes

---
+++ pythoncz_tests/models/events_test.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import ics
+
+from pythoncz.models.events import preprocess_ical
+
+from pythoncz_tests import TEST_DIR
+
+
+def test_preprocess_ical():
+    file = Path(TEST_DIR) / "static/data/invalid_ical.ics"
+
+    lines = preprocess_ical(file.read_text())
+
+    calendar = ics.Calendar(lines)
+
+    assert calendar
+    assert calendar.events[0]
+
+    # there are two alarms in the file, one type AUDIO and one type NONE
+    # NONE has to be removed because ics can't parse it
+    # the other one should not be removed
+    assert len(calendar.events[0].alarms) == 1
+    assert calendar.events[0].alarms[0].action == "AUDIO"
