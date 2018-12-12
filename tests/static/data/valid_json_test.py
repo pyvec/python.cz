677112c56f1a2924d56ecbb0bbcd84ab38d88e88
Honza Javorek <mail@honzajavorek.cz>
2018-12-12 17:30:02 +0100

    rename tests directory

--- tests/static/data/valid_json_test.py
+++ tests/static/data/valid_json_test.py
@@ -5,7 +5,7 @@ from os import path
 
 import pytest
 
-from pythoncz_tests import ROOT_DIR, DATA_DIR, generate_filenames
+from tests import ROOT_DIR, DATA_DIR, generate_filenames
 
 
 glob_patterns = [
